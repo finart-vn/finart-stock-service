c143ff570a3a0bd965dc3d11f7c767e1b450549f
Hoai Tran <hoaitran2599@gmail.com>
2025-05-12 14:31:46 +0700

    chore: update dependencies and enhance application configuration with Swagger integration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { APP_CONFIG, getAppNameAndVersion } from './config/app.config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger(APP_CONFIG.NAME);
@@ -17,6 +18,15 @@ async function bootstrap() {
   app.setGlobalPrefix(APP_CONFIG.API_PREFIX);
   
   const port = process.env.PORT ?? 3001;
+  const config = new DocumentBuilder()
+    .setTitle('Finart API')
+    .setDescription('The Finart API description')
+    .setVersion('1.0')
+    .addTag('finart')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(port);
   
   logger.log(`🚀 ${getAppNameAndVersion()} is running on http://localhost:${port}/${APP_CONFIG.API_PREFIX}`);
